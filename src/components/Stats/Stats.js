3c8e46d19c95b8a26cdab92ff42e4f3b5d1a234b
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-03-23 15:37:50 -0600

    Responsive and typography adjustments.

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -9,15 +9,15 @@ const Stats = ({ url }) => {
 	const { confirmed, deaths, recovered, lastUpdate } = stats
 	const date = new Date(lastUpdate)
 	return (
-		<div className="text-3xl py-2">
-			<h1 className="text-5xl py-2">World Stats</h1>
+		<div className="py-2">
+			<h1 className="text-5xl py-2">COVID-19 World Stats</h1>
 			<div className="sticky top-0 grid grid-cols-4 gap-2 py-4 px-6 text-sm bg-gray-900 text-gray-200">
 				<div><strong>Total Confirmed</strong></div>
 				<div><strong>Total Deaths</strong></div>
 				<div><strong>Total Recovered</strong></div>
 				<div><strong>Last Updated</strong></div>
 			</div>
-			<div className="grid grid-cols-4 gap-2 items-center bg-gray-200 hover:bg-gray-400 py-4 px-6 border-b border-l border-r border-gray-400">
+			<div className="grid grid-cols-4 gap-2 text-xl md:text-3xl items-center bg-gray-200 hover:bg-gray-400 py-4 px-6 border-b border-l border-r border-gray-400">
 				<p>{confirmed.value}</p>
 				<p>{deaths.value}</p>
 				<p>{recovered.value}</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,9 @@ import Stats from '../components/Stats/Stats'
 
 export default () => (
 	<>
-		<div className="font-xs bg-gray-900 text-gray-200 text-center p-2"><p>Data sourced from <a className="text-blue-300" href="https://github.com/mathdroid/covid-19-api" target="_blank" rel="noopener noreferrer">https://github.com/mathdroid/covid-19-api</a></p></div>
+		<div className="font-xs bg-gray-900 text-gray-200 text-center p-2">
+			<p>Data sourced from <a className="text-blue-300" href="https://github.com/mathdroid/covid-19-api" target="_blank" rel="noopener noreferrer">https://github.com/mathdroid/covid-19-api</a></p>
+		</div>
 		<Layout>
 			<Stats url="https://covid19.mathdro.id/api" />
 			<TableStats />
